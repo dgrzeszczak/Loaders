2c5ad6fdd5885858d87b54916ede203ff768f664
Dariusz Grzeszczak <d.grzeszczak@gmail.com>
2021-08-31 17:42:03 +0200

    - hide Color

--- Loaders/Sources/Color.swift
+++ Loaders/Sources/Color.swift
@@ -8,34 +8,34 @@
 import Foundation
 import UIKit
 
-public protocol Color: BundleLoader {
+/*public*/ protocol Color: BundleLoader {
     var name: String { get }
 }
-public protocol GroupedColor: Color { }
+/*public*/ protocol GroupedColor: Color { }
 
 extension Color where Self: RawRepresentable, Self.RawValue == String {
-    public var name: String { rawValue }
+    /*public*/ var name: String { rawValue }
 }
 
 extension GroupedColor where Self: RawRepresentable, Self.RawValue == String {
-    public var name: String { String(describing: Self.self) + "/" + rawValue }
+    /*public*/ var name: String { String(describing: Self.self) + "/" + rawValue }
 }
 
 extension Color where Self: RawRepresentable, Self.RawValue == String {
 
-    public var uiColor: UIColor? {
+    /*public*/ var uiColor: UIColor? {
         UIColor(named: name, in: Self.bundle, compatibleWith: nil)
     }
 
-    public func uiColor(for traitCollection: UITraitCollection) -> UIColor? {
+    /*public*/ func uiColor(for traitCollection: UITraitCollection) -> UIColor? {
         UIColor(named: name, in: Self.bundle, compatibleWith: traitCollection)
     }
 
-    public var cgColor: CGColor? {
+    /*public*/ var cgColor: CGColor? {
         uiColor?.cgColor
     }
 
-    public func cgColor(for traitCollection: UITraitCollection) -> CGColor? {
+    /*public*/ func cgColor(for traitCollection: UITraitCollection) -> CGColor? {
         uiColor(for: traitCollection)?.cgColor
     }
 }
--- LoadersTests/LoadersTests.swift
+++ LoadersTests/LoadersTests.swift
@@ -7,7 +7,7 @@
 //
 
 import XCTest
-import Loaders
+@testable import Loaders
 import SwiftUI
 
 //Todo
